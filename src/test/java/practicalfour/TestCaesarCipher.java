27a948500917a29b3f4bce0af6282a9a0850bf5d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-09-27 21:37:25 -0400

    Add the TestCaesarCipher with the TODO markers for two of the tests.

---
+++ src/test/java/practicalfour/TestCaesarCipher.java
@@ -0,0 +1,49 @@
+package practicalfour;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import practicalfour.CaesarCipher;
+
+/**
+ * A JUnit test suite for the CaesarCipher.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class TestCaesarCipher {
+
+  /** The sentence used for testing. */
+  private static String SENTENCE = "THE EAGLE IS IN PLAY; MEET AT JOE'S AT 2:00 PM.";
+
+  /** A small shift for the letters. */
+  private static int SMALL_SHIFT = 2;
+
+  /** A big shift for the letters. */
+  private static int BIG_SHIFT = 10;
+
+  /** A gigantic (full)-shift for the letters. */
+  private static int GIGANTIC_SHIFT = 26;
+
+  @Test
+  public void testEncryptAndDecryptSentenceSmall() {
+    String plaintext = SENTENCE;
+    CaesarCipher cipher = new CaesarCipher(SMALL_SHIFT);
+    String ciphertext = cipher.encrypt(plaintext);
+    String ciphertextDecrypted = cipher.decrypt(ciphertext);
+    assertEquals(plaintext, ciphertextDecrypted);
+  }
+
+  @Test
+  public void testEncryptAndDecryptSentenceBig() {
+    // TODO: Follow the pattern for the first test
+    // using the BIG_SHIFT size instead of SMALL_SHIFT
+  }
+
+  @Test
+  public void testEncryptAndDecryptSentenceGigantic() {
+    // TODO: Follow the pattern for the first test
+    // using the GIGANTIC_SHIFT size instead of SMALL_SHIFT
+}
+
+}
